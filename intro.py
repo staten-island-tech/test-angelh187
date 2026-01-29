2ff9595d1fcd10c480dd458aeb8efe2fb6c13060
angelh187 <angelh187@nycstudents.net>
2026-01-29 10:17:36 -0500

    day3

---
+++ intro.py
@@ -0,0 +1,56 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+""" t.forward(200) """
+
+""" 
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
+
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) """
+
+""" def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+ """
+""" def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle() """
+
+def equal():
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal()
+
+
+turtle.done()
---
+++ loops.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import *
+t = Turtle()
+""" 
+for i in range(4):
+    t.forward(100)
+    t.left(90) """
+""" 
+for i in range(3):
+    t.forward(100)
+    t.left(120) """
+
+
+
+turtle.done()
