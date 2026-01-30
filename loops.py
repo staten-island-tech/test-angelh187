dd4a14e9543f86d75c46c2072b414b2148575a0c
angelh187 <angelh187@nycstudents.net>
2026-01-30 10:17:40 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -5,11 +5,51 @@ t = Turtle()
 for i in range(4):
     t.forward(100)
     t.left(90) """
-""" 
-for i in range(3):
+
+
+""" for i in range(3):
     t.forward(100)
-    t.left(120) """
+    t.left(120) 
+ """
 
+""" sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90) """
 
+""" 
+for i in range(60):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.right(5) """
+
+""" 
+""" 
+def square():
+    t.forward(25)
+    t.left(90)
+    t.forward(25)
+    t.left(90)
+    t.forward(25)
+    t.left(90)
+    t.forward(25)
+    t.left(90)
+square() 
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length,90)
+    length = length * 2
+doubleSquares(5)
 
 turtle.done()
